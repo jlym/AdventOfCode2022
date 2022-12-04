407c133177f6327daf0b6b08f34e800c65e56a61
Jeffrey Lym <jeffreyalym@gmail.com>
2022-12-03 23:46:28 -0800

    Implement day 3a

--- day3a/Program.fs
+++ day3a/Program.fs
@@ -1,4 +1,28 @@
-﻿
+﻿open System;
+open System.IO;
+open System.Linq;
+open FSharp.Collections;
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let scoreLine (line: string): int =
+  let pocket1Set: Set<char> =  
+    line[..(line.Length / 2 - 1)]    
+    |> Set.ofSeq
+  let pocket2Chars: string = line[(line.Length / 2)..]
+
+  let duplicate: char = pocket2Chars.First(
+    fun c -> pocket1Set.Contains(c))
+  let duplicateInt: int = int duplicate
+
+  if Char.IsUpper duplicate then
+    duplicateInt - int 'A' + 27
+  else
+    duplicateInt - int 'a' + 1
+
+
+let lines: string[] = File.ReadAllLines("./input.txt");
+let score: int = 
+  lines
+  |> Seq.map scoreLine
+  |> Seq.sum
+
+printfn $"{score}"
