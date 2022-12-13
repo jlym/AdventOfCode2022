d5297bb7b5c70b8756b1ae355480dee33453b792
Jeffrey Lym <jeffreyalym@gmail.com>
2022-12-13 00:47:11 -0800

    Implement day 10b

---
+++ day10b/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var registerValues = new List<int>() { 1 };
+        var lines = File.ReadLines("./input.txt");
+
+        foreach (var line in lines)
+        {
+            if (line == "noop")
+            {
+                registerValues.Add(registerValues.Last());
+                continue;
+            }
+
+            var val = int.Parse(line.Substring("addx ".Length));
+            registerValues.Add(registerValues.Last());
+            registerValues.Add(registerValues.Last() + val);
+        }
+        
+        var crt = new StringBuilder();
+
+        for (var cycle = 0; cycle < 240; cycle++)
+        {
+            var x = cycle % 40;
+            var spriteMid = registerValues[cycle];
+
+            if (spriteMid >= x -1 && spriteMid <= x + 1)
+            {
+                crt.Append("#");
+            }
+            else
+            {
+                crt.Append(" ");
+            }
+
+            if (x == 39)
+            {
+                crt.AppendLine();
+            }
+        }
+
+        Console.Out.WriteLine(crt.ToString());
+    }
+}
