3182ffcc0b3593a7f21bcbd3ea83917f59a451ba
Jeffrey Lym <jeffreyalym@gmail.com>
2022-12-13 00:15:10 -0800

    Implement 10a

---
+++ day10a/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var history = new List<int>() { 1 };
+        var lines = File.ReadLines("./input.txt");
+
+        foreach (var line in lines)
+        {
+            if (line == "noop")
+            {
+                history.Add(history.Last());
+                continue;
+            }
+
+            var val = int.Parse(line.Substring("addx ".Length));
+            history.Add(history.Last());
+            history.Add(history.Last() + val);
+        }
+        
+        var sum = 0;
+        var ns = new List<int>() { 20, 60, 100, 140, 180, 220 };
+        foreach (var n in ns)
+        {
+            sum += history[n - 1] * n;
+        }
+
+        Console.Out.WriteLine(sum);
+    }
+}
